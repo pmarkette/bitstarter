61ca452668bf0a5bb3a064875680ff0bc9e25a3d
Ubuntu <ubuntu@ip-172-31-25-64.us-west-1.compute.internal>
2013-07-19 06:24:01 +0000

    Added buffer functions

--- web.js
+++ web.js
@@ -2,8 +2,9 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = fs.readFileSync('./index.html');
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
